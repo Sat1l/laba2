9b4710f319b38c3eddc9663b1ad1340c6b859d41
sat1l <66544255+Sat1l@users.noreply.github.com>
2023-11-10 13:46:40 +0300

    i think its a release huh? still have to make a jar file though

--- src/laba2/Main.java
+++ src/laba2/Main.java
@@ -13,10 +13,18 @@ public class Main {
     // https://pokemondb.net/pokedex/togekiss
     public static void main(String[] args) {
         Battle b = new Battle();
-        Omanyte omanyte = new Omanyte("rakushka", 1);
-        Illumise illumise = new Illumise("thing",1);
-        b.addAlly(omanyte);
-        b.addFoe(illumise);
+        Illumise zhuk = new Illumise("zhuk",1);
+        Omanyte rakushka = new Omanyte("rakushka", 1);
+        Omastar zloy = new Omastar("zloy", 1);
+        Togepi egg = new Togepi("egg", 1);
+        Togetic shpala = new Togetic("shpala", 1);
+        Togekiss tu34 = new Togekiss("tu34", 1);
+        b.addAlly(zhuk);
+        b.addAlly(zloy);
+        b.addAlly(shpala);
+        b.addFoe(rakushka);
+        b.addFoe(egg);
+        b.addFoe(tu34);
         b.go();
     }
 
